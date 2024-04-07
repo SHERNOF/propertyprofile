9c2ab7e842b749fd512e8fda5853166ff6ef4a7f
shernof <sherwin.nofuente@yahoo.com>
2024-04-07 23:16:25 +1000

    continue the first api

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -4,9 +4,7 @@ import Link from "next/link";
 
 async function fetchingProperties() {
   try {
-    const rest = await fetch(
-      `${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`
-    );
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
     if (!res) {
       throw new Error("Failed to fetch data");
     }
