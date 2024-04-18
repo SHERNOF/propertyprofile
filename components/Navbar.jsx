1f90c235bff40b0acbafb309e3aabd082e518db1
shernof <sherwin.nofuente@yahoo.com>
2024-04-18 23:23:25 +1000

    started the signIn button

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,24 +6,23 @@ import profileDefault from "../app/assets/images/profile.png";
 import Link from "next/link";
 import { FaGoogle } from "react-icons/fa";
 import { usePathname } from "next/navigation";
-import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Navbar = () => {
-  const { data: session } = useSession()
-  const [ providers, setProviders ] = useState(false)
+  const { data: session } = useSession();
+  const [providers, setProviders] = useState(false);
   const [isMobileMenuOpen, setisMobileMenuOpen] = useState(false);
   const [isProfileMenuOpen, setisProfileMenuOpen] = useState(false);
   const [isloggedIn, setisloggedIn] = useState(false);
   const pathname = usePathname();
 
-  useEffect(()=>{
-    const setAuthProviders = async () =>{
-      const res = await getProviders()
-      setProviders(res)
-    }
-    setAuthProviders()
-  }, [])
-  console.log(providers)
+  useEffect(() => {
+    const setAuthProviders = async () => {
+      const res = await getProviders();
+      setProviders(res);
+    };
+    setAuthProviders();
+  }, []);
 
   return (
     <nav className="bg-blue-700 border-b border-blue-500">
@@ -101,19 +100,23 @@ const Navbar = () => {
           </div>
 
           {/* <!-- Right Side Menu (Logged Out) --> */}
-        {!session && (
-          <div className="hidden md:block md:ml-6">
-            <div className="flex items-center">
-              { providers && Object.values(providers).map((provider, index)=>(
-                 <button key={index} onClick={()=>signIn(provider.id)} className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
-                 <FaGoogle className="text-white mr-2" />
-                 <span>Login or Register</span>
-               </button>
-              ))}
-               
+          {!session && (
+            <div className="hidden md:block md:ml-6">
+              <div className="flex items-center">
+                {providers &&
+                  Object.values(providers).map((provider, index) => (
+                    <button
+                      key={index}
+                      onClick={() => signIn(provider.id)}
+                      className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                    >
+                      <FaGoogle className="text-white mr-2" />
+                      <span>Login or Register</span>
+                    </button>
+                  ))}
+              </div>
             </div>
-          </div>
-        )}
+          )}
 
           {/* <!-- Right Side Menu (Logged In) --> */}
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
@@ -240,13 +243,17 @@ const Navbar = () => {
                 Add Property
               </Link>
             )}
-            {!session && (
-              <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4 transition ease-in-out duration-500">
-                {/* <i className="fa-brands fa-google mr-2"></i> */}
-                {/* <FaGoogle className="text-white mr-2" /> */}
-                <span>Login or Register</span>
-              </button>
-            )}
+            {!session &&
+              providers &&
+              Object.values(providers).map((provider, index) => (
+                <button
+                  key={index}
+                  onClick={() => signIn(provider.id)}
+                  className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                >
+                  <span>Login or Register</span>
+                </button>
+              ))}
           </div>
         </div>
       )}
--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,3 +1,6 @@
+import connectDb from "@/config/db";
+import User from "@/models/User";
+
 import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   providers: [
@@ -17,15 +20,31 @@ export const authOptions = {
     //   invoked on succewssful signin
     async signIn({ profile }) {
       // 1. connect to db
+      await connectDb();
       // 2. check if the user exist
+      const userExist = await User.findOne({ email: profile.email });
       // 3. if not then ssve user to db
+      if (!userExist) {
+        // truncate username if too long
+        const username = profile.name.slice(0, 12);
+
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        });
+      }
       // 4. return true to allow to signin
+      return true;
     },
     // Modify the session object
     async session({ session }) {
       // 1. get user from db
+      const user = await User.findOne({ email: session.user.email });
       // 2. Assign the user id to session
+      session.iser.id = user._id.toString();
       // 3. return session
+      return session;
     },
   },
 };
