8dd4152aa936a97141f678f3d8a776ec76380226
Sherwin Nofuente <sherwin.nofuente@@yahoo.com>
2024-05-19 00:10:28 +1000

    started the search function

--- app/api/bookmarks/route.js
+++ app/api/bookmarks/route.js
@@ -1,15 +1,39 @@
-import connectDB from '@/config/database';
+import connectDb from '@/config/db';
 import User from '@/models/User';
 import Property from '@/models/Property';
 import { getSessionUser } from '@/utils/getSessionUser';
 
 export const dynamic = 'force-dynamic';
 
+// GET /api/bookmarks
+export const GET = async () => {
+  try {
+    await connectDb();
 
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response('User ID is required', { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
+    // Find user in database
+    const user = await User.findOne({ _id: userId });
+
+    // Get users bookmarks
+    const bookmarks = await Property.find({ _id: { $in: user.bookmarks } });
+
+    return new Response(JSON.stringify(bookmarks), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response('Something went wrong', { status: 500 });
+  }
+};
 
 export const POST = async (request) => {
   try {
-    await connectDB();
+    await connectDb();
 
     const { propertyId } = await request.json();
 
